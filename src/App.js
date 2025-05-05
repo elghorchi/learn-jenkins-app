15ef71de8bc75b0df325608b27f5dd2a2b0a83ac
omar elghorchi <elghorchi.omar@gmail.com>
2025-05-05 20:35:42 +0200

    just a change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins on Udemy (this is just a test to trigger SCM )
         </a>
       </header>
       <p>
